7e9a2dd0a065bbf03ef17b8d1c496d3074331d23
Jannng <1780441211@qq.com>
2019-08-12 22:20:59 +0800

    md转html

---
+++ spring-blog/src/main/test/HtmlToMd.java
@@ -0,0 +1,33 @@
+import com.vladsch.flexmark.ast.Node;
+import com.vladsch.flexmark.ext.tables.TablesExtension;
+import com.vladsch.flexmark.html.HtmlRenderer;
+import com.vladsch.flexmark.parser.Parser;
+import com.vladsch.flexmark.parser.ParserEmulationProfile;
+import com.vladsch.flexmark.util.options.MutableDataSet;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * @author: Jang
+ * @date: 2019/8/11 9:44
+ * @description：
+ */
+public class HtmlToMd {
+    @Test
+    public void markdown2Html(){
+        String html = parse("## test");
+        System.out.println(html);
+    }
+
+    private String parse(String content) {
+        MutableDataSet options = new MutableDataSet();
+        options.setFrom(ParserEmulationProfile.MARKDOWN);
+        //enable table parse!
+        options.set(Parser.EXTENSIONS, Arrays.asList(TablesExtension.create()));
+        Parser parser = Parser.builder(options).build();
+        HtmlRenderer renderer = HtmlRenderer.builder(options).build();
+        Node document = parser.parse(content);
+        return renderer.render(document);
+    }
+}
